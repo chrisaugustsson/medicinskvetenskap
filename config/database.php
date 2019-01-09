644c9458cd6bb3d9fe0a8e619da41c49d8e1840e
chris.augustsson <chris.augustsson@live.se>
2019-01-09 16:32:06 +0100

    Added MySql to scrutinizer

--- config/database.php
+++ config/database.php
@@ -31,7 +31,7 @@ if (isset($_SERVER["SERVER_NAME"]) && $_SERVER["SERVER_NAME"] === "www.student.b
 }
 
 return [
-    "dsn"              => "mysql:host=127.0.0.1;dbname=anaxdb;",
+    "dsn"              => "mysql:host=127.0.0.1:3306;dbname=anaxdb;",
     "username"         => "anax",
     "password"         => "anax",
     "driver_options"   => [\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
@@ -41,8 +41,8 @@ return [
     "emulate_prepares" => false,
 
     // True to be very verbose during development
-    "verbose"         => null,
+    "verbose"         => true,
 
     // True to be verbose on connection failed
-    "debug_connect"   => false,
+    "debug_connect"   => true,
 ];
